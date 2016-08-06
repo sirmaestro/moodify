a3348a625f8351c2e655d62a8911d0338ab9972a
sirmasetro <jordanchen99@gmail.com>
2016-08-06 13:55:45 +1200

    Add project files

---
+++ helloPerson.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "Deep Sea susck my big black dick";
+document.body.innerHTML = greeter(user);
---
+++ helloPerson.ts
@@ -0,0 +1,7 @@
+function greeter(person: string) {
+    return "Hello, " + person;
+}
+
+var user = "Deep Sea susck my big black dick";
+
+document.body.innerHTML = greeter(user);
